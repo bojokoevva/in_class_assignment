3bfa6201f6ad9dc4d33c8032bc845d4c27739141
bojokoeva <bojokoeva34@gmail.com>
2025-01-30 18:15:39 -0500

    Task 1

---
+++ in_class_assignment_1.js
@@ -0,0 +1,43 @@
+// Task_1
+
+let X = 6; // 
+if (X > 5) {
+    console.log("help");
+}
+
+let expenses = 10000;
+if (expenses > 7000) {
+    console.log("High Expenses");
+} else {
+    console.log("Manageable Expenses");
+}
+
+// Task_2
+
+let score = 75;
+if (score >= 60) {
+    console.log("Passed");
+} else {
+    console.log("Failed");
+}
+
+// Task_3
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
+
+// Task_4
+
+let num = 2;
+while (num <= 10) {
+    console.log(num);
+    num += 2;
+}
+
+
+// Task_5
+
+function calculateSquare(num) {
+    return num * num;
+}
+console.log(calculateSquare(4));
